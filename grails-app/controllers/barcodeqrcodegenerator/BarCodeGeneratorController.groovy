8cc6b38811d4feafce5944fa443c4cc6d80af481
Pankaj Shinde <pankaj.shinde@krixi.com>
2014-04-04 11:55:10 +0530

    Branch 1 commit.

--- grails-app/controllers/barcodeqrcodegenerator/BarCodeGeneratorController.groovy
+++ grails-app/controllers/barcodeqrcodegenerator/BarCodeGeneratorController.groovy
@@ -39,7 +39,9 @@ class BarCodeGeneratorController {
         // step 4
         PdfContentByte cb = writer.getDirectContent();
 
-        Image image = null      // Banch 1
+        Image image = null      // Branch 1
+
+
 
 //        // EAN 13
 //        document.add(new Paragraph("Barcode EAN.UCC-13"));
